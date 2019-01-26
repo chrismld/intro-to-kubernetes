05b796379feac800a44b4249be9646d39f32444a
Christian Melendez <christianhxc@gmail.com>
2019-01-26 13:44:13 +0100

    Genesis

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# intro-to-kubernetes
-Introduction to Kubernetes
+# Introduction to Kubernetes
+
+Here's a simple Go API that returns the host's information. You can deploy the same container to any Kubernetes cluster. I use this guide to demo how to use Kubernetes locally, and then deploy the same app to AWS, GCP, and Azure.
+
+## Build Docker Image
+
+```
+docker build -t christianhxc/api-hello .
+docker run -d -p 8080:8080 christianhxc/api-hello
+docker push christianhxc/api-hello
+```
+
+## Deploy Kubernetes Objects
+```
+kubectl apply -f ../k8s/
+kubectl get all
+```
---
+++ src/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net"
+	"os"
+)
+
+func main() {
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	server := http.NewServeMux()
+	server.HandleFunc("/", hello)
+
+	log.Printf("Server listening on port %s", port)
+	err := http.ListenAndServe(":"+port, server)
+	log.Fatal(err)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	host, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Hostname: %s\n", host)
+
+	addrs, _ := net.InterfaceAddrs()
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Fprintf(w, "IP Address: %s\n", ipnet.IP.String())
+			}
+		}
+	}
+}
