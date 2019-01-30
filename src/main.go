9f34e39ffd6809f136e390126fe24280b3412c56
Christian Melendez <christianhxc@gmail.com>
2019-01-30 01:12:10 +0100

    Translated the main text in the response payload

--- src/main.go
+++ src/main.go
@@ -25,7 +25,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Hola Mundo!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 
